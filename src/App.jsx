c321a54adce0457c751f0a6747ef884807ee1ab3
bhavishya-coder <bhavishya.devil21@gmail.com>
2024-05-03 10:04:51 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import BookStore from "./BookStore";
+
+ const App = () => {
+  return <>
+  <h1 style={{textAlign:"center"}}>BestSeller Books</h1>
+  <BookStore />;
+  </>
+};
+
+export default App;
